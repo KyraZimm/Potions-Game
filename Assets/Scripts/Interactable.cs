c4aafd5d1d6c8de9dba2bf9100d8f16588a71964
Kyra <klz245@nyu.edu>
2025-05-09 14:25:22 -0700

    basic anim & drag/drop for leaves on plant draft

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] protected Rigidbody2D rb;
 
     public static Interactable HeldItem { get; private set; }
     private static Camera m_mainCam = null;
 
-    private void Awake() {
+    protected void Awake() {
         if (m_mainCam == null) m_mainCam = Camera.main;
         HeldItem = null;
     }
-    private void OnMouseDown() {
+    protected void OnMouseDown() {
         if (HeldItem != null) return;
         HeldItem = this;
+        Debug.Log("holding item");
     }
 
-    private void OnMouseDrag() {
+    protected void OnMouseDrag() {
         if (HeldItem == this) {
             Vector2 targetPos = m_mainCam.ScreenToWorldPoint(Input.mousePosition);
             targetPos = Vector2.Lerp(rb.position, targetPos, 0.5f);
@@ -26,7 +27,7 @@ public class Interactable : MonoBehaviour
         }
     }
 
-    private void OnMouseUp() {
+    protected void OnMouseUp() {
         if (HeldItem == this) {
             HeldItem = null;
         }
